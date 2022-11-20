4b72541054bd029a01001c16f184c593ff7839c9
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-11-21 00:53:17 +0300

    PW13 - Настроить ESLint

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,29 +1,8 @@
-const router = require('express').Router();
-const User = require("../models/user");
+const router = require("express").Router();
+const { getUsers, getUserById, createUser } = require("../controllers/users");
 
-// возвращает всех пользователей
-router.get("/users", (req, res) => {
-  User.find()
-    .then(users => res.send({ data: users }))
-    .catch(err => res.status(500).send({ message: "Произошла ошибка!" }));
-  console.log("GET-запрос отработал!");
-});
-
-// возвращает пользователя по _id
-router.get("/users/:userId", (req, res) => {
-  User.findById(req.params.userId)
-    .then(user => res.send({data: user}))
-    .catch(err => res.status(500).send({ message: "Произошла ошибка!" }));
-  console.log("GET-запрос :userId отработал!");
-});
-
-// создаёт пользователя
-router.post("/users", (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка!" }));
-  console.log("POST-запрос отработал!");
-});
+router.get("/users", getUsers);
+router.get("/users/:userId", getUserById);
+router.post("/users", createUser);
 
 module.exports = router;
