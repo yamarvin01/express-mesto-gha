6407de506d96bce7edbd16f0cd1a5ee57c67267f
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-11-20 19:55:24 +0300

    PW13 - Настроить ESLint

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 console.log("Hello I am Node.js");
 
 const express = require("express");
+const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const fs = require("fs");
 const path = require("path");
 
+const User = require('./src/models/user');
+const Card = require('./src/models/card');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true
+}, err => {
+  if(err) throw err;
+  console.log('Connected to MongoDB!');
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
