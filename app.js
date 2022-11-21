8a3dfff5dae81b9ee43bb9ae62f8066705752f48
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-11-21 19:09:54 +0300

    PW13 - Настроить ESLint

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log("Hello I am Node.js");
-
 const express = require("express");
 const mongoose = require("mongoose");
 const { PORT = 3000 } = process.env;
@@ -10,7 +8,7 @@ const bodyParser = require("body-parser");
 
 const Card = require("./src/models/card");
 
-const routes = require('./src/routes/users');
+const userRoutes = require("./src/routes/users");
 
 const app = express();
 
@@ -28,14 +26,42 @@ mongoose.connect(
 // TODO: временное решение авторизации
 app.use((req, res, next) => {
   req.user = {
-    _id: '637a73f1aa4c15b86afe1d74'
+    _id: "637a73f1aa4c15b86afe1d74",
   };
 
   next();
 });
 
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use("/", userRoutes);
+
+
+
+// GET /cards — возвращает все карточки
+app.get("/cards", (req, res) => {
+  Card.find()
+    .then((cards) => res.send({ data: cards }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка!" }));
+});
+
+
+
+// POST /cards — создаёт карточку
+app.post("/cards", (req, res) => {
+  const { name, link } = req.body;
+  Card.create({ name, link })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка!" }));
+});
+
+// DELETE /cards/:cardId — удаляет карточку по идентификатору
+app.delete("/cards/:cardId", (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send({ message: "Карточка удалена", data: card }))
+    .catch(() => res.send(500).send({ message: "Произошла ошибка!" }));
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,20 +3,20 @@ const User = require("../models/user");
 const getUsers = (req, res) => {
   User.find()
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка!" }));
+    .catch(() => res.status(500).send({ message: "Произошла ошибка!" }));
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка!" }));
+    .catch(() => res.status(500).send({ message: "Произошла ошибка!" }));
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка!" }));
+    .catch(() => res.status(500).send({ message: "Произошла ошибка!" }));
 };
 
 module.exports = {
