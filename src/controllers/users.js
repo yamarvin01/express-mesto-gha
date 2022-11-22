0ef3b824cffdedc93cfd777c58750fab1544764b
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-11-22 14:39:31 +0300

    PW13 - Добавить обработку ошибок

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 
-const ERROR_CODE_400 = 400; // переданы некорректные данные
+const ERROR_CODE_400 = 400; // переданы некорректные данные в методы создания карточки,
+//пользователя, обновления аватара пользователя или профиля
 const ERROR_CODE_404 = 404; // карточка или пользователь не найден
 const ERROR_CODE_500 = 500; // ошибка по-умолчанию
 
@@ -16,10 +17,12 @@ const getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ message: "Пользователь получен", data: user }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(404).send({ message: "Запрашиваемый пользователь не найден" })
+      if (err.name === "CastError") {
+        return res
+          .status(404)
+          .send({ message: "Запрашиваемый пользователь не найден" });
       }
-      return res.status(500).send({ message: "Произошла ошибка!" })
+      return res.status(500).send({ message: "Произошла ошибка!" });
     });
 };
 
@@ -27,28 +30,47 @@ const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ message: "Пользователь создан", data: user }))
-    .catch(() => res.status(500).send({ message: "Произошла ошибка!" }));
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные" });
+      }
+      // console.log(`ОШИБКА ${err.name} ${err.message}`);
+      res.status(500).send({ message: "Произошла ошибка!" });
+    });
 };
 
 const undateProfile = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name: name, about: about })
-    .then((user) =>
-      res.send({
-        message: "Пользователь обновлен, ниже старые данные",
-        data: user,
-      })
-    )
-    .catch(() => res.status(500).send({ message: "Произошла ошибка!" }));
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name: name, about: about },
+    { runValidators: true }
+  )
+    .then((user) => res.send({ message: "Пользователь обновлен, ниже старые данные", data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: `${err.message}` });
+      }
+      return res.status(500).send({ message: "Произошла ошибка!" });
+    });
 };
 
 const undateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar: avatar })
+  console.log(avatar);
+  User.findByIdAndUpdate(req.user._id, { avatar: avatar }, { runValidators: true })
     .then((user) =>
       res.send({ message: "Аватар обновлен, ниже старые данные", data: user })
     )
-    .catch(() => res.status(500).send({ message: "Произошла ошибка!" }));
+    .catch((err) => {
+      console.log(`ОШИБКА ${err.name}`);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: `${err.message}` });
+      }
+      return res.status(500).send({ message: "Произошла ошибка!" });
+    });
 };
 
 module.exports = {
--- src/models/card.js
+++ src/models/card.js
@@ -17,13 +17,10 @@ const cardSchema = new mongoose.Schema({
     ref: "user",
     required: true,
   },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
+  likes: {
+      type: [ { type: mongoose.Schema.Types.ObjectId, ref: "user" } ],
       default: [],
     },
-  ],
   createdAt: {
     type: Date,
     default: Date.now,
