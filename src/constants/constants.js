c2bc2ff334db9e75093d170a39f9205466c7ed28
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-11-23 14:42:26 +0300

    PW13 - Исправление замечаний ревью

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,11 +10,11 @@ class NotFoundError extends Error {
 }
 
 const setErrorResponse = (res, err) => {
-  if (err.name === 'ValidationError' || err.name === 'CastError') {
-    return res.status(ERROR_CODE_VALIDATION).send({ message: 'Переданы некорректные данные' });
-  }
   if (err.name === 'NotFoundError') {
     return res.status(ERROR_CODE_NOTFOUND).send({ message: `${err.message}` });
+  }
+  if (err.name === 'ValidationError' || err.name === 'CastError') {
+    return res.status(ERROR_CODE_VALIDATION).send({ message: 'Переданы некорректные данные' });
   }
   return res.status(ERROR_CODE_DEFAULT).send({ message: 'Произошла ошибка' });
 };
