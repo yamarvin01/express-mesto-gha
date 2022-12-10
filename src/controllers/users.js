431d63b31f7bdef1c1d2ae6a5a490c2b06e83e17
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-12-10 16:35:03 +0300

    PW14 - Добавить валидацию url в схему

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -95,7 +95,7 @@ const undateProfile = (req, res, next) => {
     });
 };
 
-//
+// TODO: не валидируется url
 const undateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable func-names */
 const mongoose = require('mongoose');
 const myValidator = require('validator');
@@ -36,6 +37,12 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(v) {
+        const regExp = /https?:\/\/(www\.)?[0-9a-zA-Z-]+\.[0-9a-zA-Z]+\/*[a-zA-Z0-9\/\-\_\.\+\(\)\[\]~:?#@!$&'*,;=]*#?/;
+        return regExp.test(v);
+      },
+    },
   },
 });
 
