55b5b61466e24442bed0f56242dd12d9ed10bf4a
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-12-10 21:26:53 +0300

    PW14 - Реффакторинг

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -12,11 +12,11 @@ const {
 router.get('/cards', getCards);
 
 router.post('/cards', celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().pattern(/https?:\/\/(www\.)?[0-9a-zA-Z-]+\.[0-9a-zA-Z]+\/*[a-zA-Z0-9\/\-\_\.\+\(\)\[\]~:?#@!$&'*,;=]*#?/),
-    }),
-  }), createCard);
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/https?:\/\/(www\.)?[0-9a-zA-Z-]+\.[0-9a-zA-Z]+\/*[a-zA-Z0-9\/\-\_\.\+\(\)\[\]~:?#@!$&'*,;=]*#?/),
+  }),
+}), createCard);
 
 router.delete('/cards/:cardId', deleteCardById);
 router.put('/cards/:cardId/likes', addCardLikeById);
