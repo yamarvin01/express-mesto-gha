6db91927fed7000bf52e999f5996e633553a3fd8
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-12-11 19:33:54 +0300

    PW14 - Исправить замечания Ревью

--- src/controllers/cards.js
+++ src/controllers/cards.js
@@ -58,14 +58,14 @@ const addCardLikeById = (req, res, next) => {
     .then((card) => res.send({ card }))
     .catch((err) => {
       if (err.name === 'CastError') {
-        throw new ValidationError();
+        next(new ValidationError());
       }
       if (err.name === 'NotFoundError') {
         next(err);
+      } else {
+        next(err);
       }
-      next(err);
-    })
-    .catch(next);
+    });
 };
 
 const deleteCardLikeById = (req, res, next) => {
